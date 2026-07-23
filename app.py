60d747fa987c8925b6bdd1169a3e51cee83c10d1
kirannoor112006-cmyk <kirannoor112006@gmail.com>
2026-07-23 21:39:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ if submitted:
     else:
         try:
             genai.configure(api_key=api_key)
-            model = genai.GenerativeModel('gemini-1.5-flash')
+            model = genai.GenerativeModel('gemini-pro')
 
             prompt = f"""
             You are a smart academic advisor. A student needs help planning their study schedule.
